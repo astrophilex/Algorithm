1f1683e92520ef57c872dff1dfd47b1f6088e70e
김유나 <ynkim@sslabs.io>
2023-03-10 17:37:56 +0900

    update : 조건에 맞는 사용자와 총 거래금액 조회하기

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,9 +1,17 @@
 -- 코드를 입력하세요
-SELECT user_id, nickname, concat(city,' ', street_address1,' ',street_address2) as 전체주소, 
-    concat(left(TLNO,3),'-',substring(tlno,4,4),'-',right(tlno,4)) as 전화번호
-from used_goods_board ugb
-join used_goods_user ugu
-on ugb.writer_id = ugu.user_id
-group by ugb.writer_id
-having count(ugb.writer_id) >= 3
-order by ugu.user_id DESC
+SELECT DISTINCT A.USER_ID
+        , A.NICKNAME
+        , A.ADRESS AS 전체주소
+        , A.PHONE AS 전화번호
+FROM(
+SELECT UGU.USER_ID
+            , UGU.NICKNAME
+             , CITY||' '||STREET_ADDRESS1 ||' '|| STREET_ADDRESS2 AS ADRESS
+             ,  SUBSTR(TLNO, 0,3)||'-'||SUBSTR(TLNO, 4,4)||'-'||SUBSTR(TLNO, 8,4) AS PHONE
+            , COUNT(*) OVER (PARTITION BY USER_ID) AS CNT
+FROM USED_GOODS_USER UGU
+INNER JOIN USED_GOODS_BOARD UGB
+ON UGB.WRITER_ID = UGU.USER_ID
+    ) A
+WHERE A.CNT > 2
+ORDER BY A.USER_ID DESC
