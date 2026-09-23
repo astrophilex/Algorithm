af99d808675409bb5a1599ef3342436dc7ebb9cd
Yuna Kim <yuna1206yuna5@gmail.com>
2026-09-23 20:40:34 +0900

    [level 1] Title: 최소직사각형, Time: 0.61 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,30 +1,20 @@
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int max_w = 0;
-        int max_h = 0;
-        int weight = 0;
-        int height = 0;
+        int max_w = 0; // 최대 가로 길이
+        int max_h = 0; // 최대 세로 길이
+        
+        // 가로 세로 구분없이 큰 값, 작은 값 구분해서 큰 값 중에서 큰 값, 작은 값 중에서 큰 값 구하면 됨
         
         for(int i = 0; i < sizes.length; i++){
-            for(int j = 0; j < sizes.length; j++){
-                if(sizes[i][0] > sizes[i][1]){
-                    weight = sizes[i][0];
-                    height = sizes[i][1];
-                }else{
-                    weight = sizes[i][1];
-                    height = sizes[i][0];
-                }
-                
-                if(max_w < weight){
-                 max_w = weight;    
-                }
-                if(max_h < height){
-                 max_h = height;    
-                }
-                
-                
-                
+            int weight = Math.max(sizes[i][0], sizes[i][1]); // Math.max 최댓값 구함
+            int height = Math.min(sizes[i][0], sizes[i][1]); // Math.max 최솟값 구함
+
+            if(max_w < weight){
+             max_w = weight;    
+            }
+            if(max_h < height){
+             max_h = height;    
             }
         }
         
